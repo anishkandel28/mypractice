7e16e27abdfc052b5049be1b73f67e58c213ff13
anishkandel28 <anishkandel100@gmail.com>
2022-01-11 01:10:00 +0545

    frontend

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <Container style={{ marginTop: "200px" }}>
+        
+        <h1 className="text-center text-white bg-warning p-2 mt-4">
+          NFT-SHOP
+        </h1>
+        <h2 className="m-4 text-center">
+         
+          <strong style={{ color: "red" }}>Final Assignment</strong>
+        </h2>
+        <h1>
+         
+        </h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/nft-data.js
@@ -0,0 +1,92 @@
+const nfts = [
+  {
+    name: "Beeple",
+    varients: ["image", "video", "gif" ],
+    prices: [
+      {
+        image: 99,
+        video: 199,
+        gif: 399,
+      },
+    ],
+    category: "2d",
+    image: "/images/beeple.jpg",
+    description: "Mike Winkelmann, aka the digital artist Beeple, smashed records with a Christie's auction of an NFT collage of the first 5000 entries",
+  },
+  {
+    name: "Crypto Punk",
+    varients: ["image", "video", "gif" ],
+    prices: [
+      {
+        image: 99,
+        video: 199,
+        gif: 399,
+      },
+    ],
+    category: "2d",
+    image: "/images/punk.png",
+    description:
+      "one of the first ever created. It's part of the CryptoPunks collection, a set of 10,000 NFTs released in 2017, a time when much of the world was still finding out what bitcoin is",
+  },
+  {
+    name: "Bored App",
+    varients: ["image", "video", "gif" ],
+    prices: [
+      {
+        image: 99,
+        video: 199,
+        gif: 399,
+      },
+    ],
+    category: "2d",
+    description:
+"Set of Bored Ape NFTs sells for $24.4 mln in Sotheby's online auction",
+    image: "/images/boredapp.jpg",
+  },
+  {
+    name: "Apes",
+    varients: ["image", "video", "gif" ],
+    prices: [
+      {
+        image: 99,
+        video: 199,
+        gif: 399,
+      },
+    ],
+    category: "3d",
+    image: "/images/apes.png",
+    description:
+      "NFTs of cartoon apes sell for $24.4 million at Sotheby's auction",
+  },
+  {
+    name: "Dream Lady",
+    varients: ["image", "video", "gif" ],
+    prices: [
+      {
+        image: 99,
+        video: 199,
+        gif: 399,
+      },
+    ],
+    category: "3d",
+    image: "/images/dreamlady.PNG",
+    description:
+      "The dreamy lady lives in paradise that where is colorful and full of serenity. Where every person aspires. She has no worries about anything.",
+  },
+  {
+    name: "Mr Monoster",
+    varients: ["image", "video", "gif" ],
+    prices: [
+      {
+        image: 99,
+        video: 199,
+        gif: 399,
+      },
+    ],
+    category: "3d",
+    image: "/images/Mrmonaster.png",
+    description:
+      "Mr. Monster” NFT Project Collection includes Fascinating and Unique Monsters on the NFTb",
+  },
+];
+export default nfts;
